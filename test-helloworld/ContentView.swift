834ea5b0fb60611c32797ee45461d9df4e321175
reggiegunawan88 <reggiegunawan88@gmail.com>
2021-06-04 18:21:24 +0700

    Initial Commit

---
+++ test-helloworld/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test-helloworld
+//
+//  Created by TI Digital on 04/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ test-helloworld/test_helloworldApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_helloworldApp.swift
+//  test-helloworld
+//
+//  Created by TI Digital on 04/06/21.
+//
+
+import SwiftUI
+
+@main
+struct test_helloworldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
